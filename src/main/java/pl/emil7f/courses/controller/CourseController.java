50b847c74559b59fbc0e12566e749e235e6ae160
emil7f <emilfigzal@gmail.com>
2021-03-26 20:05:47 +0100

    course controller

---
+++ src/main/java/pl/emil7f/courses/controller/CourseController.java
@@ -0,0 +1,52 @@
+package pl.emil7f.courses.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+import pl.emil7f.courses.model.Course;
+import pl.emil7f.courses.service.CourseService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<Course> getCourses() {
+        return courseService.getCourses();
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourse(@PathVariable String id) {
+        return courseService.getCourse(id);
+    }
+
+    @PostMapping
+    public Course addCourse(@Valid @RequestBody Course course){
+        return courseService.addCourse(course);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCourse(@PathVariable String id) {
+        courseService.deleteCourse(id);
+    }
+
+    @PutMapping("/{id}")
+    public Course putCourse(@PathVariable String id, @RequestBody Course course) {
+        return courseService.putCourse(id, course);
+    }
+
+    @PatchMapping("/{id}")
+    public Course patchCourse(@PathVariable String id, @RequestBody Course course) {
+        return courseService.patchCourse(id, course);
+    }
+
+
+}
---
+++ src/test/java/pl/emil7f/courses/controller/CourseControllerTest.java
@@ -0,0 +1,7 @@
+package pl.emil7f.courses.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseControllerTest {
+
+}
